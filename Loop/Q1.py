df5dbfe5a1e60879977182f1159112d68565916e
Dibyendu Biswas <67157274+dibyendubiswas1998@users.noreply.github.com>
2021-02-27 17:06:29 +0530

    Add files via upload

---
+++ Loop/Q1.py
@@ -0,0 +1,19 @@
+
+# # 1. Write a Python program to construct the following pattern, using a nested for loop.
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
+
+# for i in range(1, 6):
+#     print('*' * i)
+# for j in range(6, 1, -1):
+#     print('*' * j)
+
+
+# # 2. Write a Python program to print alphabet pattern 'A'. 
