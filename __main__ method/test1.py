b97aac74f1a0d650d1b3cbb34e1e47c389b75900
Dibyendu Biswas <67157274+dibyendubiswas1998@users.noreply.github.com>
2021-03-27 09:18:27 +0530

    Add files via upload

---
+++ __main__ method/test1.py
@@ -0,0 +1,21 @@
+
+# ex-01:--
+# print("Hello all " + __name__)
+# print("Dibyendu Biswas")
+
+
+
+def sum(a, b):
+    return(f"{a} + {b} = {a+b}")
+
+def subtract(a, b):
+    return (f"{a} - {b} = {a-b}")
+
+def String(str):
+    return f"This is:  {str}"
+
+if __name__== "__main__":
+    print(sum(30, 40))
+    print(subtract(30, 2))
+    print(String("Dibyendu Biswas"))
+    
---
+++ __main__ method/test2.py
@@ -0,0 +1,10 @@
+import test1
+
+# ex-01:--
+# print(__name__)
+
+
+print("Sum value is: " + test1.sum(20, 20))
+print("Subtract value is: " + test1.subtract(20, 20))
+print("String is: " + test1.String("Me, Okay !"))
+
